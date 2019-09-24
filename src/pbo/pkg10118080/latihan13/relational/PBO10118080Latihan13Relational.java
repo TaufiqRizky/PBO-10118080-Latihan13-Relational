3a13b217c5c89e8844b5a53f1a90c52e1c0bfd11
Taufiq R <taufiq.it2@gmail.com>
2019-09-24 20:41:36 +0700

    first commit

---
+++ src/pbo/pkg10118080/latihan13/relational/PBO10118080Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118080.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program Latihan relational
+ */
+public class PBO10118080Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1=10;
+        int bil2=20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
